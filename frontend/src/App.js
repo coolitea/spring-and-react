af9239ab161bc00a3efabcb7c3e48221fcc5d8a2
hjpark <cool.itea@gmail.com>
2019-04-16 15:11:01 +0900

    Added a rest call to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    setInterval(this.hello, 250);
+  }
+
+  hello = () => {
+    fetch("/api/hello")
+      .then(response => response.text())
+      .then(message => {
+        this.setState({ message: message });
+      });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Hello, React World!</h1>
+        <h1>{this.state.message}</h1>
+        <h2>This is React World!</h2>
       </div>
     );
   }
